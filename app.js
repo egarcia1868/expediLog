36f095423f8f0a94b64daf432c6e88de7517f111
verypaleale <55856960+verypaleale@users.noreply.github.com>
2020-03-27 14:21:35 -0500

    inital server setup

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+let db = require("./models");
+require('dotenv').config();
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("./public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
