9d0ec06d5828b4e0d6a11eaf62f4eadd355ddb0c
Eric Garcia <egarcia1868@gmail.com>
2020-03-28 11:31:04 -0500

    getting our routes working

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -27,11 +27,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// db.sequelize.sync().then(function() {
+  
+// });
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
