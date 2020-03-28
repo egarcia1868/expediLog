d68babafe195df51e4b322cae35f59799a6be934
shawn220a <shawn.tschoepe@icloud.com>
2020-03-28 10:20:55 -0500

    Add starting route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = (app) => {
 
+  app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/login.html'));
+  });
+
   app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/login.html'));
   });
