f8567676be84571a9e9961a76b8fce78a3c9b651
Eric Garcia <egarcia1868@gmail.com>
2020-03-29 21:09:27 -0500

    changing css filename to style.css as well as the references to it, adding the functioning background changer and the logo/link for the picture host site

--- public/assets/js/expedilog.js
+++ public/assets/js/expedilog.js
@@ -1,59 +1,90 @@
-$(function() {
-  $(".devourIt").on("click", function (event) {
-    let id = $(this).data("id");
-    // let nowDevoured = $(this).data("devoured");
+let background = function() {
+  // Event listener for our cat-button
 
-    var nowDevouredState = {
-      devoured: 1   //nowDevoured
-    };
+  // Storing our giphy API URL for a random cat image
+  var queryURL = "https://pixabay.com/api/?key=15801379-7b9c3ade3001b04f83cbbf0cd&q=travel&per_page=200";
 
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: nowDevouredState
-    }).then(
-      function() {
-        console.log("changed sleep to 1");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+  // Perfoming an AJAX GET request to our queryURL
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
 
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+  // After the data from the AJAX request comes back
+    .then(function(response) {
+      // console.log(response)
 
-    var newBurger = {
-      burger_name: $("#ca").val().trim(),
-      devoured: 0  //$("[name=devoured]:checked").val().trim()
-    };
+    // Saving the image_original_url property
+      const rando = Math.floor(Math.random() * 200); 
+      console.log("rando: "+rando);
+      const tinyImageURL = response.hits[rando].previewURL;
+      console.log("tinyImageURL: "+tinyImageURL);
+      const tinyImageLength = tinyImageURL.length;
+      console.log("tinyImageLength: "+tinyImageLength);
+      const imageURL = tinyImageURL.slice(0, tinyImageLength-7)+"960_720.jpg";
+      console.log("imageURL: "+imageURL)
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+      // Prepending the catImage to the images div
+      document.querySelector("body").setAttribute("style", `background-image: url("${imageURL}")`);
+    });
+}
 
-  // $(".delete-cat").on("click", (event) => {
-  //   var id = $(this).data("id");
+// $(function() {
+//   $(".devourIt").on("click", function (event) {
+//     let id = $(this).data("id");
+//     // let nowDevoured = $(this).data("devoured");
 
-  //   // Send the DELETE request.
-  //   $.ajax("/api/cats/" + id, {
-  //     type: "DELETE"
-  //   }).then(
-  //     function() {
-  //       console.log("deleted cat", id);
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     }
-  //   );
-  // });
-});
+//     var nowDevouredState = {
+//       devoured: 1   //nowDevoured
+//     };
+
+//     // Send the PUT request.
+//     $.ajax("/api/burgers/" + id, {
+//       type: "PUT",
+//       data: nowDevouredState
+//     }).then(
+//       function() {
+//         console.log("changed sleep to 1");
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+
+//   $(".create-form").on("submit", function(event) {
+//     // Make sure to preventDefault on a submit event.
+//     event.preventDefault();
+
+//     var newBurger = {
+//       burger_name: $("#ca").val().trim(),
+//       devoured: 0  //$("[name=devoured]:checked").val().trim()
+//     };
+
+//     // Send the POST request.
+//     $.ajax("/api/burgers", {
+//       type: "POST",
+//       data: newBurger
+//     }).then(
+//       function() {
+//         console.log("created new burger");
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+
+//   // $(".delete-cat").on("click", (event) => {
+//   //   var id = $(this).data("id");
+
+//   //   // Send the DELETE request.
+//   //   $.ajax("/api/cats/" + id, {
+//   //     type: "DELETE"
+//   //   }).then(
+//   //     function() {
+//   //       console.log("deleted cat", id);
+//   //       // Reload the page to get the updated list
+//   //       location.reload();
+//   //     }
+//   //   );
+//   // });
+// });
