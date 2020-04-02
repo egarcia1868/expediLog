c0f69bab2f27792317cb2ee4e60197af887ae471
Eric Garcia <egarcia1868@gmail.com>
2020-04-02 16:17:36 -0500

    fixing replicating of logs

--- public/js/expedilog.js
+++ public/js/expedilog.js
@@ -15,7 +15,6 @@ L.tileLayer(
 ).addTo(mymap);
 
 $(document).ready(function () {
-
   $('#newEntry').submit(function () {
     event.preventDefault();
 
@@ -44,7 +43,7 @@ $(document).ready(function () {
           type: 'POST',
           data: newData,
         }).then(function () {
-          $("#newEntry")[0].reset();
+          $('#newEntry')[0].reset();
           getData();
         });
       });
@@ -54,13 +53,12 @@ $(document).ready(function () {
 });
 
 async function getData() {
+  $('ul').empty();
   const response = await fetch('/api');
   const data = await response.json();
 
   console.log(response);
   console.log(data);
-  
-  
 
   for (item of data) {
     const txt1 = `<li class="entryLog" data-id="${item.id}">`;
@@ -68,7 +66,6 @@ async function getData() {
     const txt3 = `<div class="entry entryTitle"><p>${item.journal_entry}</p><button class="delete">Delete this log!</button></div></li>`;
 
     $('ul').prepend(txt1 + txt2 + txt3);
-    
 
     const marker = L.marker([item.latitude, item.longitude]).addTo(mymap);
     const txt = `${item.location}`;
@@ -76,13 +73,13 @@ async function getData() {
     marker.bindPopup(txt);
   }
   $('.delete').click(function () {
-    // console.log("fuck this")
     event.stopPropagation();
     const id = this.parentElement.parentElement.attributes[1].value;
+    console.log(id);
     // console.log(this.parentElement.parentElement.attributes[1].value)
     $.ajax({
-      method: "DELETE",
-      url: `/api/logs/${id}`
+      method: 'DELETE',
+      url: `/api/logs/${id}`,
     }).then(function () {
       getData();
     });
